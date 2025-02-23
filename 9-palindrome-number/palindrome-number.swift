5e66cca74d1da716e664259e8d221e4c18db2fe0
Bhaskara Sai Vamsi Krishna Padala <152455231+pbsvk@users.noreply.github.com>
2025-02-22 20:58:41 -0500

    Time: 7 ms (46.85%) | Memory: 19.9 MB (33.01%) - LeetSync

--- 9-palindrome-number/palindrome-number.swift
+++ 9-palindrome-number/palindrome-number.swift
@@ -1,6 +1,6 @@
 class Solution {
     func isPalindrome(_ x: Int) -> Bool {
-        guard x >= 0 else { return false}
+     
         if String(x) == String(String(x).reversed())
         {
             return true
