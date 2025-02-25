48d29ffc9cdc4ad03b3ebe3bfffd62fedad78c84
Bhaskara Sai Vamsi Krishna Padala <152455231+pbsvk@users.noreply.github.com>
2025-02-24 22:56:52 -0500

    Time: 0 ms (100.00%) | Memory: 19.7 MB (57.69%) - LeetSync

--- 389-find-the-difference/find-the-difference.swift
+++ 389-find-the-difference/find-the-difference.swift
@@ -1,14 +1,14 @@
 class Solution {
     func findTheDifference(_ s: String, _ t: String) -> Character {
-        var result = 0
+        var result: UInt8 = 0
 
-        for char in s.unicodeScalars {
-            result ^= Int(char.value)
+        for char in s.utf8 {
+            result ^= char
         }
-        for char in t.unicodeScalars {
-            result ^= Int(char.value)
+        for char in t.utf8 {
+            result ^= char
         }
 
-        return Character(UnicodeScalar(result)!)
+        return Character(UnicodeScalar(result))
     }
 }
