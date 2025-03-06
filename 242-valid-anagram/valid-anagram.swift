bca0bd5700547d01fe361c794d9e1b64600c591d
Bhaskara Sai Vamsi Krishna Padala <152455231+pbsvk@users.noreply.github.com>
2025-03-06 10:37:05 -0500

    Time: 20 ms (21.58%) | Memory: 21.1 MB (8.75%) - LeetSync

--- 242-valid-anagram/valid-anagram.swift
+++ 242-valid-anagram/valid-anagram.swift
@@ -1,7 +1,5 @@
 class Solution {
     func isAnagram(_ s: String, _ t: String) -> Bool {
-        
-            s.sorted() == t.sorted()
-        
+            s.sorted() == t.sorted()    
     }
 }
