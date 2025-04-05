4e6eeee63d315883adf3ca1dd08f6f655011c54e
Bhaskara Sai Vamsi Krishna Padala <152455231+pbsvk@users.noreply.github.com>
2025-04-05 17:31:30 -0400

    Time: 1194 ms (91.97%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,3 +1,3 @@
 -- Write your PostgreSQL query statement below
 SELECT product_name, year, price
-FROM Sales LEFT JOIN Product ON PRODUCT.product_id = Sales.product_id
+FROM Sales INNER JOIN Product ON PRODUCT.product_id = Sales.product_id
