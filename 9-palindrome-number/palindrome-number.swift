40a285a2c28a9c9fcafd43c60fd0608e9cd87fe9
Bhaskara Sai Vamsi Krishna Padala <152455231+pbsvk@users.noreply.github.com>
2025-06-01 22:23:54 -0700

    Time: 2 ms (70.18%) | Memory: 19.6 MB (85.20%) - LeetSync

--- 9-palindrome-number/palindrome-number.swift
+++ 9-palindrome-number/palindrome-number.swift
@@ -1,11 +1,11 @@
 class Solution {
     func isPalindrome(_ x: Int) -> Bool {
-        var reverse = 0
+        var r = 0
         var temp = x 
         while(temp > 0) {
-            reverse = (reverse*10) + (temp % 10)
+            r = (r*10) + (temp % 10)
             temp = temp/10
         }
-        return reverse == x
+        return r == x
     }
 }
