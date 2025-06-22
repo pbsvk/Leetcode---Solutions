46bf0ce87df864bd97ee0248727fda1e2682fc57
Bhaskara Sai Vamsi Krishna Padala <152455231+pbsvk@users.noreply.github.com>
2025-06-21 20:28:59 -0700

    Time: 0 ms (100.00%) | Memory: 19.9 MB (84.92%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.swift
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.swift
@@ -1,13 +1,7 @@
 class Solution {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
-    let len = nums.count
-    guard len > 1 else {return len}
-    var id = 0
-    for n in nums where n != nums[id]{
-        id = id+1
-        nums[id] = n
+        nums = Array(Set(nums))
+        nums.sort{$0 < $1}
+        return nums.count
     }
-        return id + 1
-
-}
 }
