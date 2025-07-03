48a7229d0086062c4564bd0c57c7226d7d0e0dbd
Bhaskara Sai Vamsi Krishna Padala <152455231+pbsvk@users.noreply.github.com>
2025-07-03 00:19:16 -0700

    Add solution for Climbing Stairs

--- solutions/easy/Climbing Stairs.swift
+++ solutions/easy/Climbing Stairs.swift
@@ -3,4 +3,11 @@
             // Language: Swift
             // Link: https://leetcode.com/problems/climbing-stairs/
 
-c
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+       let sqrt5 = sqrt(5.0)
+    }
+       let phi = (1.0 + sqrt5)/2
+       let psi = (1.0 - sqrt5)/2
+       return Int(round(((pow(phi, Double(n+1)) - pow(psi, Double(n+1))) / sqrt5)))
+}
