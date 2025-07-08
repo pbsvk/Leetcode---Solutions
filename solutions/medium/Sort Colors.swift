1ed91869b9b180722ebc98bd6157dcd66ae4a1c5
Bhaskara Sai Vamsi Krishna Padala <152455231+pbsvk@users.noreply.github.com>
2025-07-07 18:58:18 -0700

    Add solution for Sort Colors

---
+++ solutions/medium/Sort Colors.swift
@@ -0,0 +1,10 @@
+// Title: Sort Colors
+            // Difficulty: Medium
+            // Language: Swift
+            // Link: https://leetcode.com/problems/sort-colors/
+
+class Solution {
+    func sortColors(_ nums: inout [Int]) {
+        nums.sort()
+    }
+}
