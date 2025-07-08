1cf188d55d521505362efc4f4322879b45e52827
Bhaskara Sai Vamsi Krishna Padala <152455231+pbsvk@users.noreply.github.com>
2025-07-07 18:22:01 -0700

    Add solution for Sort an Array

---
+++ solutions/medium/Sort an Array.swift
@@ -0,0 +1,10 @@
+// Title: Sort an Array
+            // Difficulty: Medium
+            // Language: Swift
+            // Link: https://leetcode.com/problems/sort-an-array/
+
+class Solution {
+    func sortArray(_ nums: [Int]) -> [Int] {
+        return nums.sorted()
+    }
+}
