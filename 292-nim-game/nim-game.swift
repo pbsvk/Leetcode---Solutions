16e91deea40843230f6fb5211a559b26f1599415
Bhaskara Sai Vamsi Krishna Padala <152455231+pbsvk@users.noreply.github.com>
2025-08-10 17:58:01 -0700

    Time: 0 ms (100.00%) | Memory: 19.1 MB (86.96%) - LeetSync

--- 292-nim-game/nim-game.swift
+++ 292-nim-game/nim-game.swift
@@ -1,17 +1,5 @@
 class Solution {
     func canWinNim(_ n: Int) -> Bool {
-
-     if n <= 3 {
-        return true
-     }   
-     
-     if n % 4 == 0 {
-        return false
-     }
-     else{
-        return true
-     }
-
-     return false
+        return n % 4 != 0
     }
 }
